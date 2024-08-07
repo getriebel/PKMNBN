7682853376251ad49db12de644a2b53a77a8fcdd
Jonas Triebel <86043330+getriebel@users.noreply.github.com>
2024-08-07 09:37:29 +0200

    Create file with examples

---
+++ scripts/areas.js
@@ -0,0 +1,95 @@
+/*
+Gun
+[___][___][___]
+[___][(B)][___]
+[___][___][___]
+
+[___][___][___]
+[___][_A_][___]
+[___][___][___]
+Description: Current cell of player B if player B is in the same column
+*/
+
+/*
+Shotgun
+[___][___][___]
+[___][(B)][___]
+[___][(_)][___]
+
+[___][___][___]
+[___][_A_][___]
+[___][___][___]
+Description: Two cells in a column with depth=1, i.e. one cell in between
+*/
+
+/*
+SideGun
+[___][___][___]
+[(_)][(B)][(_)]
+[___][___][___]
+
+[___][___][___]
+[___][_A_][___]
+[___][___][___]
+Description: Three cells (2 cells if player A non-centered) in a row with depth=1, i.e. one cell in between
+*/
+
+/*
+CrossGun
+[___][(_)][___]
+[(_)][(B)][(_)]
+[___][(_)][___]
+
+[___][___][___]
+[___][_A_][___]
+[___][___][___]
+Description: Three cells (2 cells if player centered) in +-shape with depth=1, i.e. one cell in between
+*/
+
+/*
+Melee
+[___][___][___]
+[___][___][___]
+[___][(B)][___]
+
+[___][_A_][___]
+[___][___][___]
+[___][___][___]
+Description: One cell in front
+*/
+
+/*
+WideMelee
+[___][___][___]
+[___][___][___]
+[(_)][(B)][(_)]
+
+[___][_A_][___]
+[___][___][___]
+[___][___][___]
+Description: Row, i.e. three cells, in front (two cells in player not vertically centered)
+*/
+
+/*
+LongMelee
+[___][___][___]
+[___][(B)][___]
+[___][(_)][___]
+
+[___][_A_][___]
+[___][___][___]
+[___][___][___]
+Description: Two cells in front
+*/
+
+/*
+Bomb
+[___][___][___]
+[___][(B)][___]
+[___][___][___]
+
+[___][___][___]
+[___][_A_][___]
+[___][___][___]
+Description: Once cell with depth=2, i.e. two cells in between
+*/
