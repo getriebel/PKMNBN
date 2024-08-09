47e81bfe5eeaf9c1531ac914377351d435ee5fb6
Jonas Triebel <86043330+getriebel@users.noreply.github.com>
2024-08-09 09:51:52 +0200

    Complete Starter NetPkmn

--- scripts/netpkmn.js
+++ scripts/netpkmn.js
@@ -1,22 +1,44 @@
 const pokemonStats = {
     "bulbasaur": {
-        "hp": "180",
+        "hp": "450",
+        "element": "wood",
+        "bonus": "wood",
         "evolution": "ivysaur",
-        "treshold": "0.68"
+        "treshold": "0.7"
     },
     "squirtle": {
-        "hp": "210",
+        "hp": "550",
+        "element": "aqua",
+        "bonus": "aqua",
         "evolution": "wartortle",
-        "treshold": "0.68"
+        "treshold": "0.7"
     },
     "charmander": {
-        "hp": "220",
+        "hp": "500",
+        "element": "fire",
+        "bonus": "fire",
         "evolution": "charmeleon",
-        "treshold": "0.68"
+        "treshold": "0.7"
     },
     "pichu": {
-        "hp": "190",
+        "hp": "500",
+        "element": "elec",
+        "bonus": "elec",
         "evolution": "pikachu",
-        "treshold": "0.68"
+        "treshold": "0.8"
+    },
+    "pidgey": {
+        "hp": "500",
+        "element": "norm",
+        "bonus": "air",
+        "evolution": "pidgeotto",
+        "treshold": "0.6"
+    },
+    "happiny": {
+        "hp": "500",
+        "element": "norm",
+        "bonus": "heal",
+        "evolution": "chansey",
+        "treshold": "0.8"
     }
 }
